a4bc669bb5318d0b7233e74ef4205fcc31c1ae97
Deissson <86420878+Deissson@users.noreply.github.com>
2022-07-02 03:19:41 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -Sy; git clone https://github.com/Deissson/personal-arch.git; cd personal-arch/arch-script;chmod +x arch.sh;clear;./arch.sh
+pacman -Sy; git clone https://github.com/Deissson/personal-arch.git; cd personal-arch/arch-script;chmod +x arch.sh;./arch.sh
